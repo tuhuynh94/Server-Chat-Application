91a4f959242cf73836fc7695a323376752b87013
gardonzx <gardonzx@gmail.com>
2017-11-02 23:11:52 +0700

    add hConversations

---
+++ Chat_Server/NodeServer/Handlers/hConversation.js
@@ -0,0 +1,11 @@
+
+var hConversation = (function () {
+    var _load_conversation = function (io, socket, data) {
+        
+    }
+    return {
+        load_conversation: _load_conversation
+    };
+
+})();
+module.exports = hConversation;
