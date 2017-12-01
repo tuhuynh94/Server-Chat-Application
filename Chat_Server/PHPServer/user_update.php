f691f94183c340c853fec8438f785bcdc81e2014
THUONG\Thuong <thuongtruong2702@gmail.com>
2017-12-01 09:26:11 +0700

    update user

--- Chat_Server/PHPServer/user_update.php
+++ Chat_Server/PHPServer/user_update.php
@@ -19,8 +19,16 @@ $password = $_POST["password"];
 $image_source = $_POST["image_source"];
 $username = $_POST["username"];
 $email = $_POST["email"];
-
+$conversayion_id= $_POST["conversation_id"];
 $sql ="UPDATE users SET username = '".$username."', birthday = '".$birthday."', password = '".$password."',image_source = '".$image_source."',email = '".$email."' WHERE phone = '".$phone."'";
+echo $sql;
+$conn->query($sql); 
+
+$sql ="UPDATE friends SET username = '".$username."' WHERE friend_phone = '".$phone."'";
+echo $sql;
+$conn->query($sql); 
+
+$sql ="UPDATE invite_friend SET from_user = '".$username."' WHERE from_phone IN '".$phone."'";
 echo $sql;
 $conn->query($sql); 
 
